8534f29d72551e8fb227c130009fd8be6187d360
not-gato <vitorkauanrandow9@gmail.com>
2025-08-19 18:34:54 -0400

    Create PT.lua

---
+++ not-by-me/translation/PT.lua
@@ -0,0 +1,18 @@
+return {
+    WalkSpeed = "Velocidade",
+    FullBright = "Brilho Total",
+    NoFog = "Sem Névoa",
+    FootballESP = "ESP da Bola",
+    PlayerESP = "ESP de Jogador",
+    AimLockBall = "Travar Câmera no Jogador com Bola",
+    NotifyBall = "Notificar Quem Tem a Bola",
+    FOV = "Campo de Visão",
+    Language = "Idioma",
+    Player = "Jogador",
+    Camera = "Câmera",
+    ESP = "ESP",
+    Helper = "Auxiliar",
+    AimLock = "Travar Câmera",
+    Config = "Configuração",
+    BallAlert = "[ALERTA BOLA!]: %s tem a Bola!"
+}
