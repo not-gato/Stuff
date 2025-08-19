1bcf33dedc1715e1f1d23db8d5168689aca63daa
not-gato <vitorkauanrandow9@gmail.com>
2025-08-19 18:35:13 -0400

    Create ES.lua

---
+++ not-by-me/translation/ES.lua
@@ -0,0 +1,18 @@
+return {
+    WalkSpeed = "Velocidad",
+    FullBright = "Brillo Completo",
+    NoFog = "Sin Niebla",
+    FootballESP = "ESP de Balón",
+    PlayerESP = "ESP de Jugador",
+    AimLockBall = "Apuntar Jugador con Balón",
+    NotifyBall = "Notificar Quién Tiene el Balón",
+    FOV = "Campo de Visión",
+    Language = "Idioma",
+    Player = "Jugador",
+    Camera = "Cámara",
+    ESP = "ESP",
+    Helper = "Ayuda",
+    AimLock = "Apuntar",
+    Config = "Configuración",
+    BallAlert = "[ALERTA BALÓN!]: %s tiene el Balón!"
+}
