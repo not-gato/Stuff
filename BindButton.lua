fd43c64001a05a1c0053d23ee0bfd6ac79bc04b6
not-gato <vitorkauanrandow9@gmail.com>
2025-08-31 13:28:44 -0400

    Update BindButton.lua

--- BindButton.lua
+++ BindButton.lua
@@ -1,5 +1,4 @@
--- when the 🙏
-
+local M = {}
 local Players = game:GetService("Players")
 local player = Players.LocalPlayer
 
@@ -11,49 +10,42 @@ screenGui.Parent = player:WaitForChild("PlayerGui")
 local bindArea = Instance.new("Frame")
 bindArea.Name = "BindArea"
 bindArea.BorderSizePixel = 0
-bindArea.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 bindArea.BackgroundTransparency = 1
-bindArea.Size = UDim2.new(0.4, 0, 0.36, 0)
-bindArea.Position = UDim2.new(0.58879, 0, 0.02086, 0)
+bindArea.Size = UDim2.new(0.4,0,0.36,0)
+bindArea.Position = UDim2.new(0.58879,0,0.02086,0)
 bindArea.Parent = screenGui
 
-function screenGui:CreateBindable(name, callback)
+function M:CreateBindable(name, callback)
     local button = Instance.new("TextButton")
     button.Name = name or "Unknown"
     button.Text = name or "Bindable Button"
     button.TextWrapped = true
     button.TextScaled = true
     button.TextSize = 14
-    button.TextColor3 = Color3.fromRGB(255, 255, 255)
-    button.FontFace = Font.new([[rbxasset://fonts/families/Jura.json]], Enum.FontWeight.Regular, Enum.FontStyle.Normal)
-    button.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+    button.TextColor3 = Color3.fromRGB(255,255,255)
+    button.BackgroundColor3 = Color3.fromRGB(0,0,0)
     button.BackgroundTransparency = 0.7
     button.BorderSizePixel = 0
-    button.Size = UDim2.new(0, 100, 0, 100)
+    button.Size = UDim2.new(0,100,0,100)
     button.Parent = bindArea
 
-    local uicorner = Instance.new("UICorner")
-    uicorner.CornerRadius = UDim.new(0.5, 0)
-    uicorner.Parent = button
+    local uicorner = Instance.new("UICorner", button)
+    uicorner.CornerRadius = UDim.new(0.5,0)
 
-    local uistroke = Instance.new("UIStroke")
+    local uistroke = Instance.new("UIStroke", button)
     uistroke.ApplyStrokeMode = Enum.ApplyStrokeMode.Border
-    uistroke.Color = Color3.fromRGB(255, 255, 255)
-    uistroke.Parent = button
+    uistroke.Color = Color3.fromRGB(255,255,255)
 
-    local uigradient = Instance.new("UIGradient")
+    local uigradient = Instance.new("UIGradient", button)
     uigradient.Rotation = 45
     uigradient.Color = ColorSequence.new{
-        ColorSequenceKeypoint.new(0, Color3.fromRGB(0, 107, 255)),
-        ColorSequenceKeypoint.new(1, Color3.fromRGB(0, 0, 0))
+        ColorSequenceKeypoint.new(0, Color3.fromRGB(0,107,255)),
+        ColorSequenceKeypoint.new(1, Color3.fromRGB(0,0,0))
     }
-    uigradient.Parent = uistroke
 
-    if callback then
-        button.MouseButton1Click:Connect(callback)
-    end
-
+    if callback then button.MouseButton1Click:Connect(callback) end
     return button
 end
 
-return screenGui
+M.ScreenGui = screenGui
+return M
